3335d6e12ddfc7beaa4fa2fe96110d8fd2a82b27
TigranTroitskiy <120783075+TigranTroitskiy@users.noreply.github.com>
2023-01-30 20:04:36 +0300

    Add files via upload

---
+++ 01.25/1.1.sql
@@ -0,0 +1,12 @@
+use academy;
+select * from `curators`;
+select * from `departments`;
+select * from `faculties`;
+select * from `groups`;
+select * from `groups_curators`;
+select * from `groups_lectures`;
+select * from `groups_students`;
+select * from `lectures`;
+select * from `students`;
+select * from `subjects`;
+select * from `teachers`;
